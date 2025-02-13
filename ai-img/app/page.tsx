bece7e7f755e9d8e6aeb4f269e1d68f2775c75e7
Giridhar <giridhaar1105@gmail.com>
2025-02-13 19:59:25 +0530

    first commit

---
+++ ai-img/app/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import React, { useEffect, useState } from 'react';
+
+const Home: React.FC = () => {
+  const [image, setImage] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const response = await fetch('https://picsum.photos/200/300');
+      setImage(response.url);
+    };
+
+    fetchImage();
+  }, []);
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Random Image from Picsum</h1>
+        {image ? (
+          <img src={image} alt="Random" width={200} height={300} />
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
